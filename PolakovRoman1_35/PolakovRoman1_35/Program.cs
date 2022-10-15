20656646ecd1f71f113d1f7c0a3576b72eb4a0fc
RomanPolakov <113889059+RomanPolakov@users.noreply.github.com>
2022-10-15 18:18:05 +0300

    Add files via upload

---
+++ PolakovRoman1_35/PolakovRoman1_35/Program.cs
@@ -0,0 +1,10 @@
+﻿double a, b, c;
+
+Console.WriteLine("Введите a");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b");
+b = double.Parse(Console.ReadLine());
+
+c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+Console.WriteLine($"Гипотенуза = {c}");
